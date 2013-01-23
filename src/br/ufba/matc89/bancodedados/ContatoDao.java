776a2e216fe07e8a13ad45852074700f12bb3f21
Rodrigo Souza <rodrigorgs@gmail.com>
2013-01-22 21:59:31 -0300

    Codigo de BD. Exemplo completo.

--- src/br/ufba/matc89/bancodedados/ContatoDao.java
+++ src/br/ufba/matc89/bancodedados/ContatoDao.java
@@ -3,8 +3,11 @@ package br.ufba.matc89.bancodedados;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 public class ContatoDao {
 
@@ -24,15 +27,34 @@ public class ContatoDao {
 	}
 	
 	public List<Contato> getAll() {
-		// TODO
-		return new ArrayList<Contato>();
+		List<Contato> list = new ArrayList<Contato>();
+		
+		Cursor cursor = db.query("contato",
+				new String[] {"_id", "nome", "telefone"}, 
+				null, null, null, null, null);
+		
+		cursor.moveToFirst();
+		while (!cursor.isAfterLast()) {
+			long id = cursor.getLong(0);
+			String nome = cursor.getString(1);
+			String telefone = cursor.getString(2);
+			Contato contato = new Contato(id, nome, telefone);
+			list.add(contato);
+			cursor.moveToNext();
+		}
+		
+		return list;
 	}
 	
 	public void insert(Contato contato) {
-		// TODO
+		ContentValues values = new ContentValues();
+		values.put("nome", contato.getNome());
+		values.put("telefone", contato.getTelefone());
+		long id = db.insert("contato", null, values);
+		Log.d("teste", "inserido contato com id = " + id);
 	}
 	
 	public void remove(Contato contato) {
-		// TODO
+		db.delete("contato", "_id = " + contato.getId(), null);
 	}
 }

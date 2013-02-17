ae271c0b5ab5edcde922bf4799258bc2af3c646f
Rodrigo Souza <rodrigorgs@gmail.com>
2013-02-17 20:19:50 -0300

    Adiciona classe para ler JSON da internet.

---
+++ src/br/ufba/matc89/bancodedados/ContatoResource.java
@@ -0,0 +1,12 @@
+package br.ufba.matc89.bancodedados;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContatoResource {
+	public List<Contato> getContatos() {
+		List<Contato> contatos = new ArrayList<Contato>();
+		
+		return contatos;
+	}
+}

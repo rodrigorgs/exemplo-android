26eae7917f9b024ce35ddcf484659a266e716e13
Rodrigo Souza <rodrigorgs@gmail.com>
2013-02-25 16:37:49 -0300

    Melhora indentacao

--- src/br/ufba/matc89/bancodedados/MainActivity.java
+++ src/br/ufba/matc89/bancodedados/MainActivity.java
@@ -29,9 +29,9 @@ public class MainActivity extends Activity {
 				
 		contatoDao = new ContatoDao(this);
 		
-		StrictMode.ThreadPolicy policy = new StrictMode.
-				ThreadPolicy.Builder().permitAll().build();
-				StrictMode.setThreadPolicy(policy); 
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+				.permitAll().build();
+		StrictMode.setThreadPolicy(policy); 
 	}
 
 	@Override
